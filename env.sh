0245b1091bfad70d6f15c4bd2e80a1f2e40d41fa
Christopher Edward Brown <christopher.eward.brown@cern.ch>
2022-10-09 16:28:11 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate avalanche-dev-env
 
 pip install avalanche-lib
+pip install mplhep
 
 export PATH=~/anaconda3/envs/avalanche-dev-env/bin/python:$PATH
 export PYTHONPATH=$PWD:$PYTHONPATH 
